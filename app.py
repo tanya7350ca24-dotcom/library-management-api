c13de0db70a6982ddea60a48264a2bc51e5fad7e
tanya7350ca24-dotcom <tanya7350.ca24@chitkara.edu.in>
2026-06-02 03:19:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Student'
+app.config['MYSQL_DB'] = 'library_db'
+
+mysql = MySQL(app)
+
 @app.route('/')
 def home():
     try:
         cur = mysql.connection.cursor()
         cur.execute("SELECT 1")
         cur.close()
-        return "SUCCESS: Flask connected to MySQL 🚀"
+        return "Flask + MySQL working 🚀"
     except Exception as e:
-        return f"FAILED: {str(e)}"
+        return str(e)
+
+if __name__ == "__main__":
+    app.run(debug=True)
